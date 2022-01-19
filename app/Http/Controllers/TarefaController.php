7d8b4005bcc6043fded42cfe9bdad916c19fff23
Reinaldo Santos <reinaldoweb@hotmail.com>
2022-01-19 14:00:30 -0300

    App-controle-tarefas

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Mail;
+use App\Mail\NovaTarefaMail;
+use App\Models\Tarefa;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TarefaController extends Controller
+{
+ public function __construct()
+ {
+       // $this->middleware('auth');
+ }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        if(auth()->check()){
+
+            $id = auth()->user()->id;
+            $name = auth()->user()->name;
+            $email = auth()->user()->email;
+
+            return "ID: $id | Nome: $name | email: $email";
+
+        }else{
+            return 'Você não está logado no sistema';
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('tarefa.create');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $tarefa =  Tarefa::create($request->all());
+      $destinatario = Auth()->user()->email;//Email do usuario autenticado
+      Mail::to($destinatario)->send(new NovaTarefaMail($tarefa));
+
+      return redirect()->route('tarefa.show', ['tarefa' => $tarefa->id]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tarefa  $Tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tarefa $tarefa)
+    {
+        return view('tarefa.show', ['tarefa'=> $tarefa]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Tarefa  $Tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tarefa $Tarefa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tarefa  $Tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tarefa $Tarefa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tarefa  $Tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tarefa $Tarefa)
+    {
+        //
+    }
+}
