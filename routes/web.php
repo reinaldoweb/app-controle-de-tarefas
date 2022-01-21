f912464b315c53a8ebc22572f8516fc8bc10de53
Reinaldo Santos <reinaldoweb@hotmail.com>
2022-01-21 15:18:05 -0300

    Modificando a rota home

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,9 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')
-->middleware('verified');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')
+// ->middleware('verified');
+
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController')
 ->middleware('verified');
 
